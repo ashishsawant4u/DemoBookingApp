77059720551b303d4466377abc42cfbdfa142f3a
AshishS <ashishsawant4u@gmail.com>
2020-07-18 19:04:05 +0530

    Listing appointments with mock data
    Interface
    ngFor

---
+++ src/app/appointments/appointment.data.ts
@@ -0,0 +1,9 @@
+export interface AppointmentData
+{
+	appointmentNumber:string;
+	appointmentDate:Date;
+	productNumber:string;
+	customerName:string;
+	address:string;
+	custMobNo:string;
+} 
--- src/app/appointments/appointments.component.ts
+++ src/app/appointments/appointments.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { AppointmentData } from './appointment.data';
 @Component({
   selector: 'appointment-list',
   templateUrl: './appointments.component.html'
 })
 export class AppointmentsComponent 
 {
-   test:string="XXXXXXXXXXX";
+    appointmentList : AppointmentData [] = [
+        {
+          appointmentNumber:"324242344",
+          appointmentDate:new Date("12-12-2020"),
+          productNumber:"AC3380",
+          customerName:"Md Bin Zayd",
+          address:"15 Park Avenu,Aukland,NZ,2025",
+          custMobNo:"9870123456"
+        },
+        {
+          appointmentNumber:"545345354",
+          appointmentDate:new Date("25-12-2020"),
+          productNumber:"AC33820",
+          customerName:"Md Paul J",
+          address:"Midtown Park,Aukland,NZ,2027",
+          custMobNo:"9870123456"
+        }
+    ]
 }
