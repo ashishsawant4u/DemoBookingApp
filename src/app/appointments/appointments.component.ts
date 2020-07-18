26c00f12a27068a5746bbf86395140cdcf8aff43
AshishS <ashishsawant4u@gmail.com>
2020-07-18 19:16:32 +0530

    Creating Appointment Service / Registering with root / Injecting Dependency to Appointment Component
    Injectable
    Dependency Injection using constructor

--- src/app/appointments/appointments.component.ts
+++ src/app/appointments/appointments.component.ts
@@ -1,27 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppointmentData } from './appointment.data';
+import { AppointmentsService } from './appointments.service';
+
 @Component({
   selector: 'appointment-list',
   templateUrl: './appointments.component.html'
 })
-export class AppointmentsComponent 
+export class AppointmentsComponent implements OnInit
 {
-    appointmentList : AppointmentData [] = [
-        {
-          appointmentNumber:"324242344",
-          appointmentDate:new Date("12-12-2020"),
-          productNumber:"AC3380",
-          customerName:"Md Bin Zayd",
-          address:"15 Park Avenu,Aukland,NZ,2025",
-          custMobNo:"9870123456"
-        },
-        {
-          appointmentNumber:"545345354",
-          appointmentDate:new Date("25-12-2020"),
-          productNumber:"AC33820",
-          customerName:"Md Paul J",
-          address:"Midtown Park,Aukland,NZ,2027",
-          custMobNo:"9870123456"
-        }
-    ]
+    appointmentList : AppointmentData [] ;
+    appointmentsService : AppointmentsService;
+
+    constructor(appointmentsService : AppointmentsService)
+    {
+        this.appointmentsService = appointmentsService;
+    }
+
+    ngOnInit(): void {
+        
+        this.appointmentList = this.appointmentsService.getAllAppointments();
+    }
 }
---
+++ src/app/appointments/appointments.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { AppointmentData } from './appointment.data';
+
+@Injectable({
+    providedIn:"root"
+})
+export class AppointmentsService
+{
+    getAllAppointments(): AppointmentData[]
+    {
+        return [
+            {
+              appointmentNumber:"324242344",
+              appointmentDate:new Date("12-12-2020"),
+              productNumber:"AC3380",
+              customerName:"Md Bin Zayd",
+              address:"15 Park Avenu,Aukland,NZ,2025",
+              custMobNo:"9870123456"
+            },
+            {
+              appointmentNumber:"545345354",
+              appointmentDate:new Date("25-12-2020"),
+              productNumber:"AC33820",
+              customerName:"Md Paul J",
+              address:"Midtown Park,Aukland,NZ,2027",
+              custMobNo:"9870123456"
+            }
+        ]
+    }
+}
