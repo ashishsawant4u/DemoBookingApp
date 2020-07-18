ccac6b21f81edfd6cdc2dae1f62fa83433b7d7a6
AshishS <ashishsawant4u@gmail.com>
2020-07-18 18:51:24 +0530

    Creating Appointment Component

---
+++ src/app/appointments/appointments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'appointment-list',
+  templateUrl: './appointments.component.html'
+})
+export class AppointmentsComponent 
+{
+   test:string="XXXXXXXXXXX";
+}
