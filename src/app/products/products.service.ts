8fbac51b717e8a172781ba308e2f649450ae8ae7
ashishsawant4u <ashishsawant4u@gmail.com>
2020-07-21 20:05:58 +0530

    Refactoring

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -21,8 +21,8 @@ export class ProductsService
         return {
             productNumber : productNumber,
             name : "test product name",
-            description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-            imageUrl : "/assets/images/products/"+productNumber+".png",
+            description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            imageUrl : "assets/images/products/"+productNumber+".png",
             price : 2000
         }
     }
